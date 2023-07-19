dc2578995639c1db78257e4fada1260b13d94e88
ashishsinghaus <81098184+ashishsinghaus@users.noreply.github.com>
2023-07-19 15:31:13 +0000

    Updates

---
+++ enterprise-language-model/elm-api.py
@@ -0,0 +1,15 @@
+from flask import Flask, jsonify, request
+from flask_restful import Resource, Api, reqparse
+from elm import code_suggest
+
+ 
+# instance of flask application
+app = Flask(__name__)
+ 
+# home route that returns below text when root url is accessed
+@app.route("/")
+def suggestion():
+    return code_suggest()
+ 
+if __name__ == '__main__': 
+   app.run()
--- enterprise-language-model/elm.py
+++ enterprise-language-model/elm.py
@@ -1,21 +1,23 @@
+'''import librarries'''
 import os
-import openai
 import xml.etree.ElementTree as ET
+import openai
 
 def connect_to_openai():
+    '''method to connect with OpenAI API'''
     openai.organization = "org-WTPCAZdWARkJoItCjU9VV1WE"
-    openai.api_key = "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"
+    openai.api_key = "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"
     openai.Model.list()
-    MODEL = "gpt-3.5-turbo"
-    return MODEL
+    model_name = "gpt-3.5-turbo"
+    return model_name
 
-
-def get_response(hint, MODEL, lang):
+def get_response(model_input, instruction, hint_input):
+    '''method to request suggestion from OpenAI API'''
     response = openai.ChatCompletion.create(
-        model=MODEL,
+        model=model_input,
         messages=[
-            {"role": "system", "content": "Reply with only  " + lang + ", do not elaborate, do not show eaxample, do not include comments"},
-            {"role": "user", "content": hint},
+            {"role": "system", "content": instruction},
+            {"role": "user", "content": hint_input},
         ],
         temperature=0,
         max_tokens=1024,
@@ -25,24 +27,38 @@ def get_response(hint, MODEL, lang):
     )
     return response['choices'][0]['message']['content']
 
-def resolve_enterprise_vulnerability(tag_value, code):
-    if tag_value in code:
-        code = code.replace(tag_value, "")
-    return code
+def enterprise_finetuning(code_input, match_replace):
+    '''method to finetune the suggested code for enterprise'''
+    for match in match_replace:
+            if str(match) !='{}':
+                for key, value in match.items():
+                    if key in code_input:
+                        code_input=code_input.replace(key, value)
+    return code_input
 
-def read_enterprise_config(name):
-    tree = ET.parse('enterprise-config.xml')
+def read_lang_config(tag_value, lang_input):
+    '''method to read language configurations'''
+    current_dir = os.path.dirname(__file__)
+    tree = ET.parse(current_dir + "/" + lang_input + "-config.xml")
     root = tree.getroot()
-    value = root[0].text
-    return value
+    match_replace_list=[{}]
+    for child in root:
+        match_dict=child.attrib
+        match = match_dict.get(tag_value)
+        replace=child.text
+        match_replace={match: replace}
+        match_replace_list.append(match_replace)
+    return match_replace_list
 
-if __name__ == "__main__":
+
+def code_suggest():
     MODEL = connect_to_openai()
-    hint = "# write a program to generate prime numbers in python"
-    lang = "sql"
-    tag_value = read_enterprise_config("name")
-    hint=resolve_enterprise_vulnerability(tag_value, hint)
-    code = get_response(hint, MODEL, lang)
-    code = hint + "\n" + code
-    print(code)
-    
+    LANG = "python"
+    INSTRUCTION = "only code in " + LANG + ", do not elaborate, do not provide example or comment"
+    HINT = "#write a program to draw a circle"
+    code = get_response(MODEL, INSTRUCTION, HINT)
+    INSTRUCTION = "elaborate"
+    '''description = get_response(MODEL, INSTRUCTION, "Elaborate " + HINT)'''
+    attrib = read_lang_config("name", LANG)
+    code=enterprise_finetuning(code, attrib)
+    return code
